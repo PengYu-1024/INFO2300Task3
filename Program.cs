ea2e314031bd89501967abad47c0e9357c0d90d4
Peng Yu <pyu0032@conestogac.on.ca>
2021-03-20 22:04:56 -0400

    Deploy to github test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 
 namespace PYuAssignment2
 {
+    //This is the application that deployed onto cloud using Github platform
+    //For INFO2300 Task 3
+
     static class Program
     {
         /// <summary>
